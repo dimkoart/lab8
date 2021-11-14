e72014f1be97c7c3c0d90233478022db46c8554c
dimkoart <dimkoart.da@gmail.com>
2021-11-14 17:22:05 +0300

    version 15

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -16,49 +16,49 @@ public class WebDriverTest {
        
         //log in
 
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         WebElement searchInput = driver.findElement(By.xpath("/html/body/div[1]/div[1]/header/div/div[2]/div/div[2]/div[1]/div/span"));
         searchInput.click();
         //input email
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         searchInput = driver.findElement(By.cssSelector("#input-email"));
         searchInput.sendKeys("simolem.da@gmail.com");
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         //input password
         searchInput = driver.findElement(By.cssSelector("#input-password"));
         searchInput.sendKeys("Samoseyko123");
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         //press loggin button
         searchInput = driver.findElement(By.cssSelector(".button-wrapper > button:nth-child(1)"));
         searchInput.click();
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         //buuter menu
         searchInput = driver.findElement(By.cssSelector(".button\\.children_container_burger__1l-wp"));
         searchInput.click();
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         //take demo version
         searchInput = driver.findElement(By.cssSelector(".menu_listItemLink_bordered__-DkmR"));
         searchInput.click();
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         //crypto categories
         searchInput = driver.findElement(By.cssSelector(".categories_select__2fZ-8"));
         searchInput.click();
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         //sell
         searchInput = driver.findElement(By.cssSelector("option.categories_option__l305Z:nth-child(20)"));
         searchInput.click();
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         //take number of sell
         searchInput = driver.findElement(By.cssSelector("tr.watchlist-entity\\.component_row__2gfNn:nth-child(1) > td:nth-child(2) > div:nth-child(1)"));
         searchInput.click();
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         //press sell button
-        searchInput = driver.findElement(By.cssSelector(".button\\.children_container_burger__1l-wp"));
-        searchInput.click();
-        Thread.sleep(10000);
+       // searchInput = driver.findElement(By.cssSelector(".button\\.children_container_burger__1l-wp"));
+        //searchInput.click();
+        //Thread.sleep(10000);
         //open sdelki
-        searchInput = driver.findElement(By.cssSelector("li.menu_listItem__1JWUg:nth-child(4) > a:nth-child(1)"));
-        searchInput.click();
+       // searchInput = driver.findElement(By.cssSelector("li.menu_listItem__1JWUg:nth-child(4) > a:nth-child(1)"));
+       // searchInput.click();
         driver.quit();
     }
 }
