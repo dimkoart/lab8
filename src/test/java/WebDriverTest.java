ea2a5998f30669ff4159bf59608764386580e397
dimkoart <dimkoart.da@gmail.com>
2021-11-14 15:14:19 +0300

    lab8 3.0

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -1,5 +1,5 @@
-import org.junit.Test;
-import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
